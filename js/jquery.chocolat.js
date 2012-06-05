904b02fe05b4c4d6cd80a7501271e149b9ce3ba4
nicolas-t <nicolas.turlais@gmail.com>
2012-06-06 00:28:05 +0200

    Changed 'version value' to 0.3.1

--- js/jquery.chocolat.js
+++ js/jquery.chocolat.js
@@ -2,7 +2,7 @@
  Inspired by the lightbox plugin adapted to jquery by Leandro Vieira Pinho (http://leandrovieira.com)
  
  @author  : Nicolas Turlais : nicolas-at-insipi.de
- @version : V0.3 - January 2012
+ @version : V0.3.1 - June 2012
  @license : Licensed under CCAttribution-ShareAlike
  @website : http://chocolat.insipi.de
  
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
-Plugin Chocolat V0.3 pour Jquery
+Plugin Chocolat V0.3.1 pour Jquery
+http://chocolat.insipi.de
 
 Script sous licence Creative Commons Paternité - Partage des Conditions Initiales à l'Identique 2.0 France License. 
