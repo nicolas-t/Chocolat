58297ccc24c1cb2276ba5e1016bdaf127dff9345
nicolas-t <nicolas.turlais@gmail.com>
2012-12-30 14:21:02 +0100

    rewritten using jquery boilerplate

--- js/functions.js
+++ js/functions.js
@@ -1,127 +1,145 @@
-$(document).ready(function(){
-	var timeOut = false;
-	var imgOrigHeight = false;
-	var imgOrigWidth = false;
-	var fullScreen = false;
-	images = ['images/1.jpg','images/2.jpg'];
-	settings = {};
-	settings.linkImages = true;
-	settings.currentImage = false;
-	settings.lastImage = images.length - 1;
+;(function ( $, window, document, undefined ) {
 
-	load(0);
-
-	function preload(i, callback){
-		callback = tool_optFuncParam(callback);
-		imgLoader = new Image();
-		imgLoader.onload = callback(i, imgLoader);
-		imgLoader.src = images[i];
-	}
-	function load(i){
-		preload(i, appear);
-	}
-	function appear(i, imgLoader){
-		$('#img').fadeTo(400,0, function(){
-			settings.currentImage = i;
-			storeImgSize(imgLoader);
-			fitting = fit(imgOrigHeight, imgOrigWidth, $(window).height(), $(window).width(), getOutMarginH(), getOutMarginW());
-			center(fitting.width, fitting.height, fitting.left, fitting.top, 150, function(){ $('#img').attr('src', images[i]).fadeTo(400,1);});
-			arrows();
-		});
-	}
-	function fit(imgHeight, imgWidth, holderHeight, holderWidth, holderOutMarginH, holderOutMarginW)
-	{ 
-		var holderGlobalWidth = holderWidth-holderOutMarginW; 
-		var holderGlobalHeight = holderHeight-holderOutMarginH; 
-		var holderRatio = (holderGlobalHeight / holderGlobalWidth ); 
-		var imgRatio = (imgHeight / imgWidth); 
-		if(imgRatio>holderRatio) 
-		{ 
-			height = holderGlobalHeight;
-			width = height / imgRatio;
-		} 
-		else 
-		{ 
-			width = holderGlobalWidth;
-			height = width * imgRatio;
-		}
-
-		if(!fullScreen && (width>=imgWidth || height>=imgHeight)){
-			width=imgWidth;
-			height=imgHeight;
-		}
+    var pluginName = 'defaultPluginName',
+        defaults = {
+            timeOut: false,
+            imgOrigHeight: false,
+            imgOrigWidth: false,
+            fullScreen: false,
+            linkImages: true,
+            currentImage: 0,
+            lastImage: false,
+			images : ['images/1.jpg','images/2.jpg']
+		};
 		
-		return {
-			'height' :height,
-			'width' : width,
-			'top' : (holderHeight - height)/2,
-			'left' : (holderWidth - width)/2
-		}
-	}
-	function center(width, height, left, top, duration, callback){
-		callback = tool_optFuncParam(callback);		
-		$('#container').animate({
-			'width':width,
-			'height':height,
-			'left':left,
-			'top':top
-		},duration,callback()).css('overflow', 'visible');
-	}
-	function change(signe){
-		if(settings.linkImages && (settings.currentImage == 0 && signe !== -1) || (settings.currentImage == settings.lastImage && signe !== 1))
-		{
-			load(settings.currentImage + parseInt(signe));
-		}
-	}
-	function arrows(){
-		if(settings.linkImages){
-			var what = ['right','left'];
-			for(var i=0; i < what.length; i++){
-				hide = false;
-				if(what[i] == 'right' && settings.currentImage == settings.lastImage){
-					hide = true;
-					$('#'+what[i]).fadeOut(300);
-				}
-				else if(what[i] == 'left' && settings.currentImage == 0){
-					hide = true;
-					$('#'+what[i]).fadeOut(300);
-				}
-				if(!hide){
-					$('#'+what[i]).fadeIn(settings.fadeOutImageduration);
+    function Plugin( element, settings ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, settings) ;
+        this._defaults = defaults;
+        this._name = pluginName;
+		that = this;
+        this.init();
+    }
+	Plugin.prototype = {
+        init: function() {
+			$('#right').on('click', function(){
+				that.change(+1);	
+			});
+			$('#left').on('click', function(){
+				that.change(-1);	
+			});
+			$(window).on('resize', function(){
+				that.tool_debounce(100, function(){
+					fitting = that.fit(that.settings.imgOrigHeight, that.settings.imgOrigWidth, $(window).height(), $(window).width(), that.getOutMarginH(), that.getOutMarginW());
+					that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150);
+				});
+			});
+			this.settings.lastImage = this.settings.images.length - 1;
+			this.load(0);
+        }, 
+        preload: function(i, callback) {
+			callback = this.tool_optFuncParam(callback);
+			imgLoader = new Image();
+			imgLoader.onload = callback(i, imgLoader);
+			imgLoader.src = this.settings.images[i];
+        },
+		load: function(i) {
+			this.preload(i, this.appear);
+        },
+        appear: function(i, imgLoader) {
+			$('#img').fadeTo(400,0, function(){
+				that.settings.currentImage = i;
+				that.storeImgSize(imgLoader);
+				fitting = that.fit(that.settings.imgOrigHeight, that.settings.imgOrigWidth, $(window).height(), $(window).width(), that.getOutMarginH(), that.getOutMarginW());
+				that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150, function(){ $('#img').attr('src', that.settings.images[i]).fadeTo(400,1);});
+				that.arrows();
+			});
+        },
+        fit: function(imgHeight, imgWidth, holderHeight, holderWidth, holderOutMarginH, holderOutMarginW){ 
+			var holderGlobalWidth = holderWidth-holderOutMarginW; 
+			var holderGlobalHeight = holderHeight-holderOutMarginH; 
+			var holderRatio = (holderGlobalHeight / holderGlobalWidth ); 
+			var imgRatio = (imgHeight / imgWidth); 
+			
+			if(imgRatio>holderRatio) { 
+				height = holderGlobalHeight;
+				width = height / imgRatio;
+			} 
+			else { 
+				width = holderGlobalWidth;
+				height = width * imgRatio;
+			}
+
+			if(!fullScreen && (width>=imgWidth || height>=imgHeight)){
+				width=imgWidth;
+				height=imgHeight;
+			}
+			
+			return {
+				'height' :height,
+				'width' : width,
+				'top' : (holderHeight - height)/2,
+				'left' : (holderWidth - width)/2
+			}
+        },
+        center: function(width, height, left, top, duration, callback) {
+			callback = this.tool_optFuncParam(callback);		
+			$('#container').animate({
+				'width':width,
+				'height':height,
+				'left':left,
+				'top':top
+			},duration,callback()).css('overflow', 'visible');
+        },
+        change: function(signe) {
+			if(this.settings.linkImages && (this.settings.currentImage == 0 && signe !== -1) || (this.settings.currentImage == this.settings.lastImage && signe !== 1)) {
+				this.load(this.settings.currentImage + parseInt(signe));
+			}
+        },
+        arrows: function() {
+			if(this.settings.linkImages){
+				var what = ['right','left'];
+				for(var i=0; i < what.length; i++){
+					hide = false;
+					if(what[i] == 'right' && this.settings.currentImage == this.settings.lastImage){
+						hide = true;
+						$('#'+what[i]).fadeOut(300);
+					}
+					else if(what[i] == 'left' && this.settings.currentImage == 0){
+						hide = true;
+						$('#'+what[i]).fadeOut(300);
+					}
+					if(!hide){
+						$('#'+what[i]).fadeIn(this.settings.fadeOutImageduration);
+					}
 				}
 			}
-		}
-	}
-	function storeImgSize(img){
-		imgOrigHeight = img.height;
-		imgOrigWidth = img.width;
-	}
-	function getOutMarginW(){
-		return ($('#left').outerWidth() - $('#left').width()) + ($('#right').outerWidth() - $('#right').width());
-	}
-	function getOutMarginH(){
-		return $('#top').outerHeight() + $('#bottom').outerHeight();
-	}
-	function tool_debounce(duration, callback){
-		clearTimeout(timeOut);
-		timeOut = setTimeout(function(){callback();},duration);
-	}
-	function tool_optFuncParam(f){
-		if(!$.isFunction(f)){return function(){};} 
-		else{return f;}
-	}
+        },
+        storeImgSize: function(img) {
+			this.settings.imgOrigHeight = img.height;
+			this.settings.imgOrigWidth = img.width;
+        },
+        getOutMarginW: function(el, options) {
+			return ($('#left').outerWidth() - $('#left').width()) + ($('#right').outerWidth() - $('#right').width());
+        },
+        getOutMarginH: function(el, options) {
+			return $('#top').outerHeight() + $('#bottom').outerHeight();
+        },
+        tool_debounce: function(duration, callback) {
+			clearTimeout(this.timeOut);
+			this.timeOut = setTimeout(function(){callback();},duration);
+        },
+        tool_optFuncParam: function(f) {
+			if(!$.isFunction(f)){return function(){};} 
+			else{return f;}
+        }
+    };
 	
-	/*events*/
-	$('#right').on('click', function(){
-		change(+1);	
-	});
-	$('#left').on('click', function(){
-		change(-1);	
-	});
-	$(window).on('resize', function(){
-		tool_debounce(100, function(){
-			fitting = fit(imgOrigHeight, imgOrigWidth, $(window).height(), $(window).width(), getOutMarginH(), getOutMarginW());
-			center(fitting.width, fitting.height, fitting.left, fitting.top, 150);
-		});
-	});
-});
+	$.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
+    }
+})( jQuery, window, document );
