609559e8aa22b4c4bcc4d52809426c000a7a88aa
nicolas-t <nicolas.turlais@gmail.com>
2012-12-31 17:46:14 +0100

    markup and events created using js

--- js/functions.js
+++ js/functions.js
@@ -1,52 +1,46 @@
 ;(function ( $, window, document, undefined ) {
 
-    var pluginName = 'Chocolat',
-        defaults = {
-            timeOut: false,
-            imgOrigHeight: false,
-            imgOrigWidth: false,
-            fullScreen: false,
-            linkImages: true,
-            currentImage: 0,
-            lastImage: false,
-			images : ['images/1.jpg','images/2.jpg']
+	var pluginName = 'Chocolat';
+	var defaults = {
+			container:  	 'body',
+			next: 			 '#right',
+			prev: 			 '#left',
+			timeOut:		 false,
+			imgOrigHeight:	 false,
+			imgOrigWidth:	 false,
+			fullScreen:      false,
+			linkImages:		 true,
+			currentImage:	 0,
+			lastImage: 		 false,
+			images :		 ['images/1.jpg','images/2.jpg']
 		};
 		
-    function Plugin( element, settings ) {
-        this.element = element;
-        this.settings = $.extend( {}, defaults, settings) ;
-        this._defaults = defaults;
-        this._name = pluginName;
+	function Plugin( element, settings ) {
+		this.element = element;
+		this.settings = $.extend( {}, defaults, settings) ;
+		this._defaults = defaults;
+		this._name = pluginName;
 		that = this;
-        this.init();
-    }
+		
+		this.init();
+	}
 	Plugin.prototype = {
-        init: function() {
-			$('#right').on('click', function(){
-				that.change(+1);	
-			});
-			$('#left').on('click', function(){
-				that.change(-1);	
-			});
-			$(window).on('resize', function(){
-				that.tool_debounce(100, function(){
-					fitting = that.fit(that.settings.imgOrigHeight, that.settings.imgOrigWidth, $(window).height(), $(window).width(), that.getOutMarginH(), that.getOutMarginW());
-					that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150);
-				});
-			});
+		init: function() {
+			this.markup();
+			this.events();
 			this.settings.lastImage = this.settings.images.length - 1;
 			this.load(0);
-        }, 
-        preload: function(i, callback) {
+		}, 
+		preload: function(i, callback) {
 			callback = this.tool_optFuncParam(callback);
 			imgLoader = new Image();
 			imgLoader.onload = callback(i, imgLoader);
 			imgLoader.src = this.settings.images[i];
-        },
+		},
 		load: function(i) {
 			this.preload(i, this.appear);
-        },
-        appear: function(i, imgLoader) {
+		},
+		appear: function(i, imgLoader) {
 			$('#img').fadeTo(400,0, function(){
 				that.settings.currentImage = i;
 				that.storeImgSize(imgLoader);
@@ -54,8 +48,8 @@
 				that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150, function(){ $('#img').attr('src', that.settings.images[i]).fadeTo(400,1);});
 				that.arrows();
 			});
-        },
-        fit: function(imgHeight, imgWidth, holderHeight, holderWidth, holderOutMarginH, holderOutMarginW){ 
+		},
+		fit: function(imgHeight, imgWidth, holderHeight, holderWidth, holderOutMarginH, holderOutMarginW){ 
 			var holderGlobalWidth = holderWidth-holderOutMarginW; 
 			var holderGlobalHeight = holderHeight-holderOutMarginH; 
 			var holderRatio = (holderGlobalHeight / holderGlobalWidth ); 
@@ -81,8 +75,8 @@
 				'top' : (holderHeight - height)/2,
 				'left' : (holderWidth - width)/2
 			}
-        },
-        center: function(width, height, left, top, duration, callback) {
+		},
+		center: function(width, height, left, top, duration, callback) {
 			callback = this.tool_optFuncParam(callback);		
 			$('#container').animate({
 				'width':width,
@@ -90,13 +84,13 @@
 				'left':left,
 				'top':top
 			},duration,callback()).css('overflow', 'visible');
-        },
-        change: function(signe) {
+		},
+		change: function(signe) {
 			if(this.settings.linkImages && (this.settings.currentImage == 0 && signe !== -1) || (this.settings.currentImage == this.settings.lastImage && signe !== 1)) {
 				this.load(this.settings.currentImage + parseInt(signe));
 			}
-        },
-        arrows: function() {
+		},
+		arrows: function() {
 			if(this.settings.linkImages){
 				if(this.settings.currentImage == this.settings.lastImage){
 					$('#right').fadeOut(300);
@@ -111,32 +105,56 @@
 					$('#left').fadeIn(300);
 				}
 			}
-        },
-        storeImgSize: function(img) {
+		},
+		storeImgSize: function(img) {
 			this.settings.imgOrigHeight = img.height;
 			this.settings.imgOrigWidth = img.width;
-        },
-        getOutMarginW: function(el, options) {
+		},
+		getOutMarginW: function(el, options) {
 			return ($('#left').outerWidth() - $('#left').width()) + ($('#right').outerWidth() - $('#right').width());
-        },
-        getOutMarginH: function(el, options) {
+		},
+		getOutMarginH: function(el, options) {
 			return $('#top').outerHeight() + $('#bottom').outerHeight();
-        },
-        tool_debounce: function(duration, callback) {
+		},
+		markup: function(){
+			$(this.settings.container).append('\
+			<div id="container">\
+				<img src="" id="img" alt=""/>\
+				<div id="top"></div>\
+				<div id="left"></div>\
+				<div id="right"></div>\
+				<div id="bottom"></div>\
+			</div>');
+		},
+		events : function(){
+			$(this.settings.next).off('click').on('click', function(){
+				that.change(+1);	
+			});
+			$(this.settings.prev).off('click').on('click', function(){
+				that.change(-1);	
+			});
+			$(window).off('resize').on('resize', function(){
+				that.tool_debounce(100, function(){
+					fitting = that.fit(that.settings.imgOrigHeight, that.settings.imgOrigWidth, $(window).height(), $(window).width(), that.getOutMarginH(), that.getOutMarginW());
+					that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150);
+				});
+			});
+		},
+		tool_debounce: function(duration, callback) {
 			clearTimeout(this.timeOut);
 			this.timeOut = setTimeout(function(){callback();},duration);
-        },
-        tool_optFuncParam: function(f) {
+		},
+		tool_optFuncParam: function(f) {
 			if(!$.isFunction(f)){return function(){};} 
 			else{return f;}
-        }
-    };
+		}
+	};
 	
 	$.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
-            }
-        });
-    }
+		return this.each(function () {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+			}
+		});
+	}
 })( jQuery, window, document );
