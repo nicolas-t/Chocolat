502d965e115fd00ed592aabe7cccd4933ad925bb
nicolas-t <nicolas.turlais@gmail.com>
2013-01-06 15:11:08 +0100

    made displayAsAlink working

--- js/chocolat.jquery.js
+++ js/chocolat.jquery.js
@@ -2,18 +2,22 @@
 	var pluginName = 'Chocolat';
 	var calls = 0;
 	var defaults = {
-			container:  	 'body',
+			container:  	 window,
 			next: 			 '#right',
 			prev: 			 '#left',
+			displayAsALink:  true,
+			linksContainer:	 '#links',
 			setIndex:		 0,
 			setTitle:		 '',
-			timeOut:		 false,
-			fullWindow:      false,
-			fullScreen:      false,
+			fullWindow:      true,
+			fullScreen:      true,
 			linkImages:		 true,
 			currentImage:	 0,
 			overlayOpacity : 0.5,
+			separator1    : '|',
+			separator2    : '/',
 			timer: 			 false,
+			timerDebounce:		 false,
 			lastImage: 		 false,
 			images :		 []
 		};
@@ -21,7 +25,8 @@
 	function Plugin( element, settings ) {
 		that = this;
 		this.element = element;
-		this.settings = $.extend( {}, defaults, settings) ;
+		this.settings = settings;
+		stg = this.settings;
 		this._defaults = defaults;
 		this._name = pluginName;
 		this.init();
@@ -30,33 +35,35 @@
 		init: function() {
 			this.markup();
 			this.events();
-			$('#overlay').fadeTo(800, this.settings.overlayOpacity)
-			this.settings.lastImage = this.settings.images.length - 1;
-			this.load(this.settings.currentImage);
+			$('#overlay, #close').fadeTo(800, stg.overlayOpacity)
+			stg.lastImage = stg.images.length - 1;
+			this.load(stg.currentImage);
 		}, 
 		preload: function(i, callback) {
 			callback = this.tool_optFuncParam(callback);
 			imgLoader = new Image();
 			imgLoader.onload = callback(i, imgLoader);
-			imgLoader.src = this.settings.images[i].src;
+			imgLoader.src = stg.images[i].src;
 		},
 		load: function(i) {
-			this.settings.timer = setTimeout(function(){$('#loader').fadeIn();},400);
+			stg.timer = setTimeout(function(){$('#loader').fadeIn();},400);
 			this.preload(i,this.place);
 		},
 		place: function(i, imgLoader) {
+			stg.currentImage = i;
+			that.description();
+			that.pagination();
 			$('#img').fadeTo(200,0, function(){
 				that.storeImgSize(imgLoader, i);
-				fitting = that.fit(that.settings.images[i].height, that.settings.images[i].width, $(window).height(), $(window).width(), that.getOutMarginH(), that.getOutMarginW());
+				fitting = that.fit(stg.images[i].height, stg.images[i].width, $(stg.container).height(), $(stg.container).width(), that.getOutMarginH(), that.getOutMarginW());
 				that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150, that.appear(i));
 				that.arrows();
 			});
 		},
 		appear:function(i){
-			clearTimeout(that.settings.timer);
-			that.settings.currentImage = i;
+			clearTimeout(stg.timer);
 			$('#loader').stop().fadeOut(300, function(){
-				$('#img').attr('src', that.settings.images[i].src).fadeTo(400,1);
+				$('#img').attr('src', stg.images[i].src).fadeTo(400,1);
 			});
 		},
 		fit: function(imgHeight, imgWidth, holderHeight, holderWidth, holderOutMarginH, holderOutMarginW){ 
@@ -77,6 +84,9 @@
 				width=imgWidth;
 				height=imgHeight;
 			}
+			console.log(holderHeight);			
+			console.log(holderWidth);			
+
 			return {
 				'height' :height,
 				'width' : width,
@@ -85,7 +95,7 @@
 			}
 		},
 		center: function(width, height, left, top, duration, callback) {
-			callback = this.tool_optFuncParam(callback);		
+			callback = this.tool_optFuncParam(callback);
 			$('#container').animate({
 				'width':width,
 				'height':height,
@@ -94,19 +104,19 @@
 			},duration,callback()).css('overflow', 'visible');
 		},
 		change: function(signe) {
-			if(this.settings.linkImages && ((this.settings.currentImage !== 0 && signe == -1) || (this.settings.currentImage !== this.settings.lastImage && signe == 1))) {
-				this.load(this.settings.currentImage + parseInt(signe));
+			if(stg.linkImages && ((this.settings.currentImage !== 0 && signe == -1) || (stg.currentImage !== stg.lastImage && signe == 1))) {
+				this.load(stg.currentImage + parseInt(signe));
 			}
 		},
 		arrows: function() {
-			if(this.settings.linkImages){
-				if(this.settings.currentImage == this.settings.lastImage){
+			if(stg.linkImages){
+				if(stg.currentImage == stg.lastImage){
 					$('#right').fadeOut(300);
 				}
 				else{
 					$('#right').fadeIn(300);
 				}
-				if(this.settings.currentImage == 0){
+				if(stg.currentImage == 0){
 					$('#left').fadeOut(300);
 				}
 				else{
@@ -116,10 +126,23 @@
 				$('#left, #right').css('display','none');
 			}
 		},
+		description : function(){
+			$('#description').fadeTo(200, 0, function(){
+				$(this).html(stg.images[stg.currentImage].title).fadeTo(400,1);
+			});
+		},
+		pagination : function(){
+			var last = stg.lastImage + 1;
+			var position = stg.currentImage + 1;
+			var separator = (stg.setTitle == '') ? '' : stg.separator1;
+			$('#pagination').fadeTo(200, 0, function(){
+				$(this).html(stg.setTitle + ' ' + separator + ' ' + position + stg.separator2 + last).fadeTo(400,1);
+			});
+		},
 		storeImgSize: function(img, i) {
-			if(!this.settings.images[i].height || !this.settings.images[i].width){
-				this.settings.images[i].height = img.height;
-				this.settings.images[i].width = img.width;
+			if(!stg.images[i].height || !stg.images[i].width){
+				stg.images[i].height = img.height;
+				stg.images[i].width = img.width;
 			}
 		},
 		close:function(){
@@ -134,19 +157,31 @@
 			return $('#top').outerHeight() + $('#bottom').outerHeight();
 		},
 		markup: function(){
-			$(this.settings.container).append('\
+			var container = ( typeof stg.container === 'object') ? 'body' : stg.container;
+			$(container).append('\
 			<div id="overlay"></div>\
 			<div id="loader"></div>\
 			<div id="container">\
 				<img src="" id="img" alt=""/>\
-				<div id="top"></div>\
+				<div id="top">\
+					<span id="close">close</span>\
+				</div>\
 				<div id="left"></div>\
 				<div id="right"></div>\
-				<div id="bottom"></div>\
+				<div id="bottom">\
+					<span id="description"></span>\
+					<span id="pagination"></span>\
+				</div>\
 			</div>');
+			if(this.settings.fullScreen){
+				$('#top').prepend('<span id="fullscreen">fullscreen</span>');
+			}
+			if(this.settings.fullScreen !== window){
+				$('#overlay, #container').css('position','absolute');
+			}
 		},
 		openFullScreen:function(){
-			if(this.settings.fullScreen){
+			if(stg.fullScreen){
 				var docElm = document.documentElement;
 				if (docElm.requestFullscreen) {
 					docElm.requestFullscreen();
@@ -173,25 +208,28 @@
 					break;
 				};
 			});
-			$(this.settings.next).off('click').on('click', function(){
+			$(stg.next).off('click').on('click', function(){
 				that.change(+1);	
 			});
-			$(this.settings.prev).off('click').on('click', function(){
+			$(stg.prev).off('click').on('click', function(){
 				that.change(-1);	
 			});
-			$('#overlay').off('click').on('click', function(){
+			$('#overlay, #close').off('click').on('click', function(){
 				that.close();	
+			});
+			$('#fullscreen').off('click').on('click', function(){
+				that.openFullScreen();	
 			});
 			$(window).off('resize').on('resize', function(){
 				that.tool_debounce(100, function(){
-					fitting = that.fit(that.settings.images[that.settings.currentImage].height, that.settings.images[that.settings.currentImage].width, $(window).height(), $(window).width(), that.getOutMarginH(), that.getOutMarginW());
+					fitting = that.fit(stg.images[stg.currentImage].height, stg.images[stg.currentImage].width, $(stg.container).height(), $(stg.container).width(), that.getOutMarginH(), that.getOutMarginW());
 					that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150);
 				});
 			});
 		},
 		tool_debounce: function(duration, callback) {
-			clearTimeout(this.timeOut);
-			this.timeOut = setTimeout(function(){callback();},duration);
+			clearTimeout(stg.timerDebounce);
+			stg.timerDebounce = setTimeout(function(){callback();},duration);
 		},
 		tool_optFuncParam: function(f) {
 			if(!$.isFunction(f)){return function(){};} 
@@ -210,16 +248,24 @@
 				width : false
 			})
 		});
-		options = $.extend(options, {setIndex:calls, images : img, setTitle : ''});
-
-		//attach init event
-		this.each(function (i) {
-			$(this).off('click').on('click', function(event){
-				event.preventDefault();
-				$.data(this, 'plugin_' + pluginName, new Plugin( this, $.extend(options, {currentImage : i})));
-			});
-		});
+		settings = $.extend({}, defaults, options, {setIndex:calls, images : img} );
 		
+		//attach init event, not the best code written so far
+		if(settings.displayAsALink){
+			$(settings.linksContainer).append('<li><a href="#" id="Choco_numsetIndex_'+settings.setIndex+'" class="Choco_link">'+settings.setTitle+'</a></li>');
+			$(this).remove();
+			$('#Choco_numsetIndex_'+settings.setIndex).off('click').on('click', function(event){
+				event.preventDefault();
+				$.data(this, 'plugin_' + pluginName, new Plugin( this, $.extend(settings, {currentImage : 0})));
+			});
+		}else{
+			this.each(function (i) {
+				$(this).off('click').on('click', function(event){
+					event.preventDefault();
+					$.data(this, 'plugin_' + pluginName, new Plugin( this, $.extend(settings, {currentImage : i})));
+				});
+			});
+		}
 		return this;
 	}
 })( jQuery, window, document );
