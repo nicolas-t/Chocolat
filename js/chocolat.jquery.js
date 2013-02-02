9caa0fdba155e239fb17e545043ae31c81603a7e
nicolas-t <nicolas.turlais@gmail.com>
2013-02-03 00:37:55 +0100

    stored markup in jquery objects

--- js/chocolat.jquery.js
+++ js/chocolat.jquery.js
@@ -2,24 +2,25 @@
 	var pluginName = 'Chocolat';
 	var calls = 0;
 	var defaults = {
-			container:  	 '#cont',
-			next: 			 '#chocolat-right',
-			prev: 			 '#chocolat-left',
-			displayAsALink:  true,
-			linksContainer:	 '#links',
-			setIndex:		 0,
-			setTitle:		 '',
-			fullWindow:      false,
-			fullScreen:      false,
-			linkImages:		 true,
-			currentImage:	 0,
-			overlayOpacity : 0.5,
-			separator1    : '|',
-			separator2    : '/',
-			timer: 			 false,
-			timerDebounce:		 false,
-			lastImage: 		 false,
-			images :		 []
+			container:  		window,
+			next: 			 	'#chocolat-right',
+			prev: 			 	'#chocolat-left',
+			displayAsALink:  	false,
+			linksContainer:	 	'#links',
+			setIndex:		 	0,
+			setTitle:		 	'',
+			fullWindow:      	false,
+			fullScreen:      	false,
+			linkImages:			true,
+			currentImage:	 	0,
+			overlayOpacity : 	0.5,
+			separator1    : 	'|',
+			separator2    : 	'/',
+			timer: 				false,
+			timerDebounce:		false,
+			lastImage: 			false,
+			elems : 			{},
+			images :			[]
 		};
 		
 	function Plugin( element, settings ) {
@@ -35,7 +36,7 @@
 		init: function() {
 			this.markup();
 			this.events();
-			$('#chocolat-overlay').fadeTo(800, stg.overlayOpacity)
+			stg.elems.overlay.fadeTo(800, stg.overlayOpacity)
 			stg.lastImage = stg.images.length - 1;
 			this.load(stg.currentImage);
 		}, 
@@ -46,24 +47,31 @@
 			imgLoader.src = stg.images[i].src;
 		},
 		load: function(i) {
-			stg.timer = setTimeout(function(){$('#chocolat-loader').fadeIn();},400);
+			stg.timer = setTimeout(function(){stg.elems.loader.fadeIn();},400);
 			this.preload(i,this.place);
 		},
 		place: function(i, imgLoader) {
 			stg.currentImage = i;
 			that.description();
 			that.pagination();
-			$('#chocolat-img').fadeTo(200,0, function(){
+			stg.elems.img.fadeTo(200,0, function(){
 				that.storeImgSize(imgLoader, i);
-				fitting = that.fit(stg.images[i].height, stg.images[i].width, $(stg.container).height(), $(stg.container).width(), that.getOutMarginH(), that.getOutMarginW());
+				fitting = that.fit(
+					stg.images[i].height,
+					stg.images[i].width,
+					$(stg.container).height(),
+					$(stg.container).width(),
+					that.getOutMarginH(),
+					that.getOutMarginW()
+				);
 				that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150, that.appear(i));
 				that.arrows();
 			});
 		},
 		appear:function(i){
 			clearTimeout(stg.timer);
-			$('#chocolat-loader').stop().fadeOut(300, function(){
-				$('#chocolat-img').attr('src', stg.images[i].src).fadeTo(400,1);
+			stg.elems.loader.stop().fadeOut(300, function(){
+				stg.elems.img.attr('src', stg.images[i].src).fadeTo(400,1);
 			});
 		},
 		fit: function(imgHeight, imgWidth, holderHeight, holderWidth, holderOutMarginH, holderOutMarginW){ 
@@ -80,13 +88,10 @@
 				width = holderGlobalWidth;
 				height = width * imgRatio;
 			}
-			if(!this.settings.fullWindow && (width>=imgWidth || height>=imgHeight)){
+			if(!this.settings.fullWindow && (width >= imgWidth || height >= imgHeight)){
 				width=imgWidth;
 				height=imgHeight;
 			}
-			console.log(holderHeight);			
-			console.log(holderWidth);			
-
 			return {
 				'height' :height,
 				'width' : width,
@@ -96,7 +101,7 @@
 		},
 		center: function(width, height, left, top, duration, callback) {
 			callback = this.tool_optFuncParam(callback);
-			$('#chocolat-container').animate({
+			stg.elems.content.animate({
 				'width':width,
 				'height':height,
 				'left':left,
@@ -111,23 +116,23 @@
 		arrows: function() {
 			if(stg.linkImages){
 				if(stg.currentImage == stg.lastImage){
-					$('#chocolat-right').fadeOut(300);
+					stg.elems.right.fadeOut(300);
 				}
 				else{
-					$('#chocolat-right').fadeIn(300);
+					stg.elems.right.fadeIn(300);
 				}
 				if(stg.currentImage == 0){
-					$('#chocolat-left').fadeOut(300);
+					stg.elems.left.fadeOut(300);
 				}
 				else{
-					$('#chocolat-left').fadeIn(300);
+					stg.elems.left.fadeIn(300);
 				}
 			}else{
-				$('#chocolat-left, #chocolat-right').css('display','none');
+				$([stg.elems.left[0],stg.elems.right[0]]).css('display','none');
 			}
 		},
 		description : function(){
-			$('#chocolat-description').fadeTo(200, 0, function(){
+			stg.elems.description.fadeTo(200, 0, function(){
 				$(this).html(stg.images[stg.currentImage].title).fadeTo(400,1);
 			});
 		},
@@ -135,7 +140,7 @@
 			var last = stg.lastImage + 1;
 			var position = stg.currentImage + 1;
 			var separator = (stg.setTitle == '') ? '' : stg.separator1;
-			$('#chocolat-pagination').fadeTo(200, 0, function(){
+			stg.elems.pagination.fadeTo(200, 0, function(){
 				$(this).html(stg.setTitle + ' ' + separator + ' ' + position + stg.separator2 + last).fadeTo(400,1);
 			});
 		},
@@ -146,38 +151,91 @@
 			}
 		},
 		close:function(){
-			$('#chocolat-overlay, #chocolat-loader, #chocolat-container').fadeOut(200, function(){
+			$([stg.elems.overlay[0],stg.elems.loader[0],stg.elems.content[0]]).fadeOut(200, function(){
 				$(this).remove();
 			});
 		},
 		getOutMarginW: function(el, options) {
-			return ($('#chocolat-left').outerWidth() - $('#chocolat-left').width()) + ($('#chocolat-right').outerWidth() - $('#chocolat-right').width());
+			return (stg.elems.left.outerWidth() - stg.elems.left.width()) + (stg.elems.right.outerWidth() - stg.elems.right.width());
 		},
 		getOutMarginH: function(el, options) {
-			return $('#chocolat-top').outerHeight() + $('#chocolat-bottom').outerHeight();
+			return stg.elems.top.outerHeight() + stg.elems.bottom.outerHeight();
 		},
 		markup: function(){
 			var container = ( typeof stg.container === 'object') ? 'body' : stg.container;
-			$(container).append('\
-			<div id="chocolat-overlay"></div>\
-			<div id="chocolat-loader"></div>\
-			<div id="chocolat-container">\
-				<img src="" id="chocolat-img" alt=""/>\
-				<div id="chocolat-top">\
-					<span id="chocolat-close"></span>\
-				</div>\
-				<div id="chocolat-left"></div>\
-				<div id="chocolat-right"></div>\
-				<div id="chocolat-bottom">\
-					<span id="chocolat-description"></span>\
-					<span id="chocolat-pagination"></span>\
-				</div>\
-			</div>');
-			if(this.settings.fullScreen){
-				$('#chocolat-top').prepend('<span id="chocolat-fullscreen"></span>');
+			stg.elems.overlay = $('<div/>',{
+				'id' : 'chocolat-overlay'
+			}).appendTo(container);
+
+			stg.elems.loader = $('<div/>',{
+				'id' : 'chocolat-loader'
+			}).appendTo(container);
+
+			stg.elems.content = $('<div/>',{
+				'id' : 'chocolat-container'
+			}).appendTo(container);
+
+			stg.elems.img = $('<img/>',{
+				'id' : 'chocolat-img',
+				'src' : ''
+			}).appendTo(stg.elems.content);
+
+			stg.elems.top = $('<div/>',{
+				'id' : 'chocolat-top'
+			}).appendTo(stg.elems.content);
+
+			stg.elems.left = $('<div/>',{
+				'id' : 'chocolat-left'
+			}).appendTo(stg.elems.content);
+
+			stg.elems.right = $('<div/>',{
+				'id' : 'chocolat-right'
+			}).appendTo(stg.elems.content);
+
+			stg.elems.bottom = $('<div/>',{
+				'id' : 'chocolat-bottom'
+			}).appendTo(stg.elems.content);
+
+			stg.elems.description = $('<span/>',{
+				'id' : 'chocolat-description'
+			}).appendTo(stg.elems.bottom);
+
+			stg.elems.pagination = $('<span/>',{
+				'id' : 'chocolat-pagination'
+			}).appendTo(stg.elems.bottom);
+
+			stg.elems.close = $('<span/>',{
+				'id' : 'chocolat-close'
+			}).appendTo(stg.elems.top);
+
+			/* HTML MARKUP MEMO
+			<div id="chocolat-overlay"></div>
+			<div id="chocolat-loader"></div>
+			<div id="chocolat-container">
+				<img src="" id="chocolat-img" alt=""/>
+				<div id="chocolat-top">
+					<span id="chocolat-close"></span>
+				</div>
+				<div id="chocolat-left"></div>
+				<div id="chocolat-right"></div>
+				<div id="chocolat-bottom">
+					<span id="chocolat-description"></span>
+					<span id="chocolat-pagination"></span>
+				</div>
+			</div>
+			*/
+
+			if(stg.fullScreen){
+				stg.elems.fullscreen = $('<span/>',{
+					'id' : 'chocolat-fullscreen'
+				})
+				.off('click').on('click', function(){
+					that.openFullScreen();	
+				})
+				.appendTo(stg.elems.top);
 			}
-			if(this.settings.fullScreen !== window){
-				$('#chocolat-overlay, #chocolat-container').css('position','absolute');
+			if(stg.container !== window){
+				$([stg.elems.overlay[0], stg.elems.content[0]]).css('position','absolute');
 			}
 		},
 		openFullScreen:function(){
@@ -214,15 +272,19 @@
 			$(stg.prev).off('click').on('click', function(){
 				that.change(-1);	
 			});
-			$('#chocolat-overlay, #chocolat-close').off('click').on('click', function(){
+			$([stg.elems.overlay[0], stg.elems.close[0]]).off('click').on('click', function(){
 				that.close();	
-			});
-			$('#chocolat-fullscreen').off('click').on('click', function(){
-				that.openFullScreen();	
 			});
 			$(window).off('resize').on('resize', function(){
 				that.tool_debounce(100, function(){
-					fitting = that.fit(stg.images[stg.currentImage].height, stg.images[stg.currentImage].width, $(stg.container).height(), $(stg.container).width(), that.getOutMarginH(), that.getOutMarginW());
+					fitting = that.fit(	
+						stg.images[stg.currentImage].height,
+						stg.images[stg.currentImage].width,
+						$(stg.container).height(),
+						$(stg.container).width(),
+						that.getOutMarginH(),
+						that.getOutMarginW()
+					);
 					that.center(fitting.width, fitting.height, fitting.left, fitting.top, 150);
 				});
 			});
@@ -250,14 +312,26 @@
 		});
 		settings = $.extend({}, defaults, options, {setIndex:calls, images : img} );
 		
-		//attach init event, not the best code written so far
 		if(settings.displayAsALink){
-			$(settings.linksContainer).append('<li><a href="#" id="chocolat-numsetIndex_'+settings.setIndex+'" class="chocolat-link">'+settings.setTitle+'</a></li>');
-			$(this).remove();
-			$('#chocolat-numsetIndex_'+settings.setIndex).off('click').on('click', function(event){
+			var li = $('<li/>',{
+				'id' : 'chocolat-overlay'
+			}).appendTo(settings.linksContainer);
+
+			var link = $('<a/>',{
+				'id' : 'chocolat-numsetIndex_'+settings.setIndex,
+				'href' : '#',
+				'class' : 'chocolat-link'
+			})
+			.html(settings.setTitle)
+			.off('click')
+			.on('click', function(event){
 				event.preventDefault();
 				$.data(this, 'plugin_' + pluginName, new Plugin( this, $.extend(settings, {currentImage : 0})));
-			});
+			})
+			.appendTo(li);
+
+			$(this).remove();
+
 		}else{
 			this.each(function (i) {
 				$(this).off('click').on('click', function(event){
