399cf1c73712832788b324a4617bd55c780a18f5
Nicolas Turlais <nicolas.turlais@gmail.com>
2018-11-21 20:09:20 +0100

    Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	bracketSpacing: true,
+	useTabs: false,
+	printWidth: 100,
+	semi: false,
+	arrowParens: 'always',
+	singleQuote: true
+}
